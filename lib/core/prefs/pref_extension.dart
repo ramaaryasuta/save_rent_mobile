f34d5e69371dc09404fb78de9730beb1fe641a95
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2025-09-22 17:51:59 +0700

    feat : add prefs local

---
+++ lib/core/prefs/pref_extension.dart
@@ -0,0 +1,48 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'pref_keys.dart';
+
+Future<void> setRememberMe(bool value) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setBool(PrefKeys.rememberMe, value);
+}
+
+Future<bool> getRememberMe() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getBool(PrefKeys.rememberMe) ?? false;
+}
+
+Future<void> setEmail(String value) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString(PrefKeys.email, value);
+}
+
+Future<String> getEmail() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString(PrefKeys.email) ?? '';
+}
+
+Future<void> setPassword(String value) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString(PrefKeys.password, value);
+}
+
+Future<String> getPassword() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString(PrefKeys.password) ?? '';
+}
+
+Future<void> setToken(String value) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString(PrefKeys.token, value);
+}
+
+Future<String?> getToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString(PrefKeys.token);
+}
+
+Future<void> clearToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove(PrefKeys.token);
+}
---
+++ lib/core/prefs/pref_keys.dart
@@ -0,0 +1,8 @@
+class PrefKeys {
+  PrefKeys._();
+
+  static const rememberMe = 'remember_me';
+  static const email = 'email';
+  static const password = 'password';
+  static const token = 'token';
+}
