5228fd4161e069622aaa40fb11dcbbc6f291d1d2
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2025-09-22 17:52:09 +0700

    feat : add new routing

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -1,5 +1,7 @@
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/ui/screen/login_screen.dart';
+import '../../features/auth/ui/screen/register_screen.dart';
 import '../../features/home/ui/screen/home_screen.dart';
 import '../../features/splash/splash_screen.dart';
 import 'app_route_path.dart';
@@ -10,6 +12,14 @@ GoRouter appRoute = GoRouter(
     GoRoute(
       path: AppRoutePath.splashRoute,
       builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: AppRoutePath.loginRoute,
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: AppRoutePath.registerRoute,
+      builder: (context, state) => const RegisterScreen(),
     ),
     GoRoute(
       path: AppRoutePath.homeRoute,
--- lib/core/routing/app_route_path.dart
+++ lib/core/routing/app_route_path.dart
@@ -2,5 +2,9 @@ class AppRoutePath {
   AppRoutePath._();
 
   static const String splashRoute = '/splash';
+
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+
   static const String homeRoute = '/';
 }
