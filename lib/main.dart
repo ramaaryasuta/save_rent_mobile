c56143b571ba1cef4a3a994e7b0c7cc531374a17
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2025-09-22 17:52:54 +0700

    feat : env setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,29 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'core/routing/app_route.dart';
 import 'core/services/app_theme_service.dart';
 import 'core/theme/app_theme.dart';
+import 'features/auth/ui/provider/auth_provider.dart';
 import 'features/home/ui/provider/home_provider.dart';
 import 'utils/print_log.dart';
 
 void main() {
   runZonedGuarded(
-    () {
+    () async {
       WidgetsFlutterBinding.ensureInitialized();
+
+      // load ENV file
+      await dotenv.load();
+
       runApp(
         MultiProvider(
           providers: [
             ChangeNotifierProvider(create: (_) => AppThemeProvider()),
             ChangeNotifierProvider(create: (_) => HomeProvider()),
+            ChangeNotifierProvider(create: (_) => AuthProvider()),
           ],
           child: const MyApp(),
         ),
